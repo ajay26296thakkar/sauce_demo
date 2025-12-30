f474f578c4c1077b2b9631ee65f36f6a192664ee
ajay26296thakkar <42107393+ajay26296thakkar@users.noreply.github.com>
2025-12-30 18:22:42 +0530

    Create testData.ts

---
+++ test-data/testData.ts
@@ -0,0 +1,12 @@
+export const testConfig = {
+  credentials: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+
+  checkoutInfo: {
+    firstName: 'Ajay',
+    lastName: 'Thakkar',
+    postalCode: '388001',
+  },
+};
